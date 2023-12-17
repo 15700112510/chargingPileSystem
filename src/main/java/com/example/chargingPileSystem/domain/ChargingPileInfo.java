86a573c335a17c9c8aab2a3306bf7649260ec9c5
宋哲代 <1456253413@qq.com>
2023-12-17 19:30:48 +0800

    git merge --allow-unrelated-histories <branch-name>

--- src/main/java/com/example/chargingPileSystem/domain/ChargingPileInfo.java
+++ src/main/java/com/example/chargingPileSystem/domain/ChargingPileInfo.java
@@ -9,15 +9,25 @@ public class ChargingPileInfo {
     private int id;
     @PropertyIgnore
     private String chargingPileId;
+    //电压
     private String voltage;
+    //电流
     private String current;
+    //功率
     private String power;
+    //累计电量
     private String accumulatedElectricEnergy;
+    //状态
     private String stage;
+    //警告
     private int error;
+    //预约时间
     private String appointmentTime;
+    //设备温度
     private int equipmentTemperature;
+    //状态
     private int status;
+    //蓝牙名称
     private String bleName;
 
 }
