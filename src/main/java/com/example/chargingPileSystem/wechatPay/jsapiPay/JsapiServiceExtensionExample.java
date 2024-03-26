6a33e8da1bec5b1a2752b7f6ef9fb08de2fef08e
宋哲代 <1456253413@qq.com>
2024-03-26 10:40:15 +0800

    git merge --allow-unrelated-histories <branch-name>

--- src/main/java/com/example/chargingPileSystem/wechatPay/jsapiPay/JsapiServiceExtensionExample.java
+++ src/main/java/com/example/chargingPileSystem/wechatPay/jsapiPay/JsapiServiceExtensionExample.java
@@ -1,21 +1,19 @@
-package com.example.chargingPileSystem.wechatPay.nativepay;
+package com.example.chargingPileSystem.wechatPay.jsapiPay;
 
 import com.wechat.pay.java.core.Config;
 import com.wechat.pay.java.core.RSAAutoCertificateConfig;
 import com.wechat.pay.java.core.exception.HttpException;
 import com.wechat.pay.java.core.exception.MalformedMessageException;
 import com.wechat.pay.java.core.exception.ServiceException;
+import com.wechat.pay.java.service.payments.jsapi.JsapiServiceExtension;
+import com.wechat.pay.java.service.payments.jsapi.model.CloseOrderRequest;
+import com.wechat.pay.java.service.payments.jsapi.model.PrepayRequest;
+import com.wechat.pay.java.service.payments.jsapi.model.PrepayWithRequestPaymentResponse;
+import com.wechat.pay.java.service.payments.jsapi.model.QueryOrderByIdRequest;
+import com.wechat.pay.java.service.payments.jsapi.model.QueryOrderByOutTradeNoRequest;
 import com.wechat.pay.java.service.payments.model.Transaction;
-import com.wechat.pay.java.service.payments.nativepay.NativePayService;
-import com.wechat.pay.java.service.payments.nativepay.model.CloseOrderRequest;
-import com.wechat.pay.java.service.payments.nativepay.model.PrepayRequest;
-import com.wechat.pay.java.service.payments.nativepay.model.PrepayResponse;
-import com.wechat.pay.java.service.payments.nativepay.model.QueryOrderByIdRequest;
-import com.wechat.pay.java.service.payments.nativepay.model.QueryOrderByOutTradeNoRequest;
 
-/** NativePayService使用示例 */
-public class NativePayServiceExample {
-
+public class JsapiServiceExtensionExample {
   /** 商户号 */
   public static String merchantId = "190000****";
   /** 商户API私钥路径 */
@@ -25,7 +23,7 @@ public class NativePayServiceExample {
   /** 商户APIV3密钥 */
   public static String apiV3Key = "...";
 
-  public static NativePayService service;
+  public static JsapiServiceExtension service;
 
   public static void main(String[] args) {
     // 初始化商户配置
@@ -37,12 +35,16 @@ public class NativePayServiceExample {
             .merchantSerialNumber(merchantSerialNumber)
             .apiV3Key(apiV3Key)
             .build();
-
     // 初始化服务
-    service = new NativePayService.Builder().config(config).build();
-    // ... 调用接口
+    service =
+        new JsapiServiceExtension.Builder()
+            .config(config)
+            .signType("RSA") // 不填默认为RSA
+            .build();
     try {
-      closeOrder();
+      // ... 调用接口
+      PrepayWithRequestPaymentResponse response = prepayWithRequestPayment();
+      System.out.println(response);
     } catch (HttpException e) { // 发送HTTP请求失败
       // 调用e.getHttpRequest()获取请求打印日志或上报监控，更多方法见HttpException定义
     } catch (ServiceException e) { // 服务返回状态小于200或大于等于300，例如500
@@ -59,12 +61,12 @@ public class NativePayServiceExample {
     // 调用接口
     service.closeOrder(request);
   }
-  /** Native支付预下单 */
-  public static PrepayResponse prepay() {
+  /** JSAPI支付下单，并返回JSAPI调起支付数据 */
+  public static PrepayWithRequestPaymentResponse prepayWithRequestPayment() {
     PrepayRequest request = new PrepayRequest();
     // 调用request.setXxx(val)设置所需参数，具体参数可见Request定义
     // 调用接口
-    return service.prepay(request);
+    return service.prepayWithRequestPayment(request);
   }
   /** 微信支付订单号查询订单 */
   public static Transaction queryOrderById() {
