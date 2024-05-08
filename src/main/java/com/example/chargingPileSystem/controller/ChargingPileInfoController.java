5a8d6971b201fc6745cf3041f38a2669e7ddb50b
梁琛悦 <3205310481@qq.com>
2024-05-08 17:07:54 +0800

    Merge branch 'master' of https://gitee.com/songzhedai-dynasty/chargingPileSystem

--- src/main/java/com/example/chargingPileSystem/controller/ChargingPileInfoController.java
+++ src/main/java/com/example/chargingPileSystem/controller/ChargingPileInfoController.java
@@ -22,5 +22,33 @@ public class ChargingPileInfoController {
     public R<?> updateChargingPrice(@RequestParam String chargingPileId,@RequestParam Double chargingPrice) throws Exception {
         return chargingPileInfoService.updateChargingPrice(chargingPileId,chargingPrice);
     }
+    //1.获取当前所有充电桩
+    @GetMapping("/all")
+    public R<?> ChargingPileInfo() {
+        return chargingPileInfoService.ChargingPileInfo();
+    }
 
+    //2.新增充电桩
+    @PostMapping("/insert")
+    public R<?> InsertInfo(@RequestBody String chargingPileInfo) {
+        return chargingPileInfoService.insertChargingPile(chargingPileInfo);
+    }
+
+    //3.获取当前充电桩状态
+    @GetMapping("/status")
+    public R<?> ChargingPileStatus(@RequestBody String ChargingPileId) {
+        return chargingPileInfoService.getChargingPileStatus(ChargingPileId);
+    }
+
+    //4.获取当前机床充电情况
+    @PostMapping("/charging")
+    public R<?> InsertCont(@RequestBody String ChargingPileId) {
+        return chargingPileInfoService.getCharging(ChargingPileId);
+    }
+
+    //5.删除机器
+    @DeleteMapping ("/delete")
+    public R<?> DeleteChargingPile(@RequestParam String ChargingPileId) {
+        return chargingPileInfoService.deleteChargingPile(ChargingPileId);
+    }
 }
